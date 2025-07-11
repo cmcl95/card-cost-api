eb0a46b87f96cc1e14279ff7f92b83532ee7aeb2
cmcl95 <cmcl9512@gmail.com>
2025-07-11 17:11:20 -0300

    Force the application's locale to be Locale.US to ensure all messages are in English.

--- src/main/java/com/cmcabrera/cardcostapi/CardCostApiApplication.java
+++ src/main/java/com/cmcabrera/cardcostapi/CardCostApiApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Locale;
+
 @SpringBootApplication
 @EnableJpaRepositories("com.cmcabrera.cardcostapi.repository")
 @EntityScan("com.cmcabrera.cardcostapi.entity")
 public class CardCostApiApplication {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         SpringApplication.run(CardCostApiApplication.class, args);
     }
 
