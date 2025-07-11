2a3e744da51f04132f74af4a4339defd8e5b3949
cmcl95 <cmcl9512@gmail.com>
2025-07-11 15:31:19 -0300

    Add dependencies and configuration for H2, Lombok and MapStruct.

--- src/main/java/com/cmcabrera/cardcostapi/CardCostApiApplication.java
+++ src/main/java/com/cmcabrera/cardcostapi/CardCostApiApplication.java
@@ -2,8 +2,12 @@ package com.cmcabrera.cardcostapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.cmcabrera.cardcostapi.repository")
+@EntityScan("com.cmcabrera.cardcostapi.entity")
 public class CardCostApiApplication {
 
     public static void main(String[] args) {
