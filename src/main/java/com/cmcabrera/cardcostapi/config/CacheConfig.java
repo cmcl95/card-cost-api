9d564a3a075411eec528d8e71a75edcbfd567145
cmcl95 <cmcl9512@gmail.com>
2025-07-12 17:40:44 -0300

    Add Redis (L2 cache) dependencies and configuration.

--- src/main/java/com/cmcabrera/cardcostapi/config/CacheConfig.java
+++ src/main/java/com/cmcabrera/cardcostapi/config/CacheConfig.java
@@ -2,20 +2,41 @@ package com.cmcabrera.cardcostapi.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.support.CompositeCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@EnableCaching
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("binCache");
-        cacheManager.setCaffeine(caffeineCacheBuilder());
-        return cacheManager;
+    @Primary
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("binCache");
+        caffeineCacheManager.setCaffeine(caffeineCacheBuilder());
+
+        RedisCacheManager redisCacheManager = RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration())
+                .build();
+
+        return new CompositeCacheManager(caffeineCacheManager, redisCacheManager);
+    }
+
+    private RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(24))
+                .disableCachingNullValues();
     }
 
     Caffeine<Object, Object> caffeineCacheBuilder() {
