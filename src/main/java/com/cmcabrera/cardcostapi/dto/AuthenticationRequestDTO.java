502bb5b65dfb546b162cdb3ce598f65cf23c6019
cmcl95 <cmcl9512@gmail.com>
2025-07-12 23:28:15 -0300

    Add Authentication endpoint DTOs.

---
+++ src/main/java/com/cmcabrera/cardcostapi/dto/AuthenticationRequestDTO.java
@@ -0,0 +1,13 @@
+package com.cmcabrera.cardcostapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequestDTO {
+    private String username;
+    private String password;
+}
