4a8caf615911e0a161f33f5eb2d56d8cbdc8cf90
cmcl95 <cmcl9512@gmail.com>
2025-07-12 23:34:20 -0300

    Initialize data with a test user.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+INSERT INTO users (username, password) VALUES
+('test-user', '$2a$12$3qhcXoek5F5ag4egmBwquedrEIZk9eAaj0xVYbCu8dGhE6J/kFSN2');
+
 INSERT INTO clearing_costs (country_code, cost) VALUES
 ('US', 5.00),
 ('GR', 15.00),
