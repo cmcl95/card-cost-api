746c506568cc08b5e53fa2b9545c1b7f23041a68
cmcl95 <cmcl9512@gmail.com>
2025-07-12 23:32:14 -0300

    Add Auth controller with an 'authenticate' endpoint.

---
+++ src/main/java/com/cmcabrera/cardcostapi/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.cmcabrera.cardcostapi.controller;
+
+import com.cmcabrera.cardcostapi.dto.AuthenticationRequestDTO;
+import com.cmcabrera.cardcostapi.dto.AuthenticationResponseDTO;
+import com.cmcabrera.cardcostapi.service.UserDetailsServiceImpl;
+import com.cmcabrera.cardcostapi.util.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/authenticate")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(AuthenticationManager authenticationManager, UserDetailsServiceImpl userDetailsService, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequestDTO authenticationRequestDTO) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequestDTO.getUsername(), authenticationRequestDTO.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequestDTO.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponseDTO(jwt));
+    }
+}
